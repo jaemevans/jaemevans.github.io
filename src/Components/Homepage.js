ff0adb8b3056c02b8780194700b02ba8131a327e
jaeevans <jaelyne2@illinois.edu>
2023-01-30 15:18:11 -0600

    trying this git thing again

---
+++ src/Components/Homepage.js
@@ -0,0 +1,17 @@
+import './Homepage.css';
+
+function Homepage() {
+  return (
+    <div className='homepagemain'>
+     <h1 className='name'>My name is Jaelyn Evans</h1>
+     <p>blaaaaah</p>
+     <p>blaaaaah</p>
+     <p>blaaaaah</p>
+     <p>blaaaaah</p>
+     <p>blaaaaah</p>
+     <p>blaaaaah</p>
+    </div>
+  );
+}
+
+export default Homepage;
