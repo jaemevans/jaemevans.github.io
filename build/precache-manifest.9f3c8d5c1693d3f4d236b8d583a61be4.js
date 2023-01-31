ac8209514ad45e194906189e66c4be9f56288b09
jaeevans <jaelynevans1@gmail.com>
2023-01-30 19:29:42 -0600

    fixed build script in package.json file

---
+++ build/precache-manifest.9f3c8d5c1693d3f4d236b8d583a61be4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ad62f046c5580db76c0",
+    "url": "/static/css/main.30b46235.chunk.css"
+  },
+  {
+    "revision": "0ad62f046c5580db76c0",
+    "url": "/static/js/main.0ad62f04.chunk.js"
+  },
+  {
+    "revision": "f8c89fafb583dff59e70",
+    "url": "/static/js/1.f8c89faf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dbbca253e699020c6274eec86c181b83",
+    "url": "/index.html"
+  }
+];
