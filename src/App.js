8b5089ab5d624188dcac50fc46195cb5d7d6ae05
jaeevans <jaelynevans1@gmail.com>
2023-01-30 19:16:52 -0600

    final commit for the night ive got 361 homework

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react'
 import Homepage from './Components/Homepage';
 
 function App() {
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,15 +1,12 @@
 import './Homepage.css';
+import React from 'react'
+// import Navigation from './Shared/Navigation';
 
 function Homepage() {
   return (
     <div className='homepagemain'>
      <h1 className='name'>My name is Jaelyn Evans</h1>
-     <p>blaaaaah</p>
-     <p>blaaaaah</p>
-     <p>blaaaaah</p>
-     <p>blaaaaah</p>
-     <p>blaaaaah</p>
-     <p>blaaaaah</p>
+     <p>"Destruction leads to a very rough road but it also breeds creation"- Anthony Kiedis, Californication</p>
     </div>
   );
 }
