54ce1528748522307fb867127824f310ab6be6d1
jaeevans <jaelynevans1@gmail.com>
2023-02-13 14:44:48 -0600

    creating contact page ui

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,14 +4,21 @@ import React from "react";
 function Contact() {
   return (
     <div className="testing">
-      <div>
+      <div className="hello">
         <h1>get in touch.</h1>
       </div>
-      <div>
+      <div className="blab">
         <p>
           see something wrong on my website? think my ui needs some work? wanna
-          hire me? then please feel free to reach out via email.
+          hire me? check out my music taste? feel free to connect with me on the
+          various platforms below
         </p>
+      </div>
+      <div className="socials">
+        <img src="images/gmail.png" alt="its me yall" />
+        <img src="images/linkedin.png" alt="its me yall" />
+        <img src="images/github.png" alt="its me yall" />
+        <img src="images/spotify.png" alt="its me yall" />
       </div>
     </div>
   );
