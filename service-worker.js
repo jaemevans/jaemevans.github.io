4ff3208ae8e01fc14f0abe563d603f24117412ed
jaeevans <jaelynevans1@gmail.com>
2023-02-14 02:31:55 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ec3e0962526c55c678d538c5461cab0.js"
+  "/precache-manifest.a89c9aa4467e33d9f06463404cbe1085.js"
 );
 
 workbox.clientsClaim();
