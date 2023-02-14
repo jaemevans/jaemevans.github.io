f5dc8f210b60e9453f6a29dc681e1b4bdad5ed74
jaeevans <jaelynevans1@gmail.com>
2023-02-14 02:33:09 -0600

    another build added

---
+++ build/precache-manifest.a89c9aa4467e33d9f06463404cbe1085.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aee03b7f1d01c9ce8372",
+    "url": "/static/css/main.1193e29d.chunk.css"
+  },
+  {
+    "revision": "aee03b7f1d01c9ce8372",
+    "url": "/static/js/main.aee03b7f.chunk.js"
+  },
+  {
+    "revision": "7f8223be229e019fcf8b",
+    "url": "/static/js/1.7f8223be.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b4649ac1bd597c4d3b306864b25242d",
+    "url": "/index.html"
+  }
+];
