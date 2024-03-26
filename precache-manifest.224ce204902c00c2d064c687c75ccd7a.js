51e418e5a9bf970b7e1798997f17f748b9c4cb90
Jaelyn Evans <mgwh46@motorolasolutions.com>
2024-03-26 13:28:22 -0500

    Updates

---
+++ precache-manifest.224ce204902c00c2d064c687c75ccd7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51982ce55049e4f44143",
+    "url": "/static/css/main.17c9d4c6.chunk.css"
+  },
+  {
+    "revision": "51982ce55049e4f44143",
+    "url": "/static/js/main.140a18ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c5f5a6eefea35bc4ee7c",
+    "url": "/static/js/2.f19d36cf.chunk.js"
+  },
+  {
+    "revision": "353fcd449329c75ef41e237aa8b91a28",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d85a2cc72f417c06a10aba71405096d.js"
+  "/precache-manifest.224ce204902c00c2d064c687c75ccd7a.js"
 );
 
 workbox.clientsClaim();
